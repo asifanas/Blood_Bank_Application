bac2a4563691eb0d784365ad786279da566631cb
asifanas <asifanas00786@gmail.com>
2022-06-03 16:25:40 +0530

    Initial commit

---
+++ app/src/main/java/opensource/celento/bloodbank/Home.java
@@ -0,0 +1,32 @@
+package opensource.celento.bloodbank;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ImageView add=(ImageView) findViewById(R.id.imageView);
+        ImageView search=(ImageView) findViewById(R.id.imageView2);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this,MainActivity.class));
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this,searchview.class));
+            }
+        });
+    }
+}
